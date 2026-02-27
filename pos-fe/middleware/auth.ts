38b49d7df01f2f2138f087e29d72ac14a8e42cbe
John Paul Frace <jpfrace@princetech.com.ph>
2026-02-27 19:37:48 +0800

    Clearing

--- pos-fe/middleware/auth.ts
+++
@@ -1,6 +0,0 @@
-export default defineNuxtRouteMiddleware((to) => {
-  const { isLoggedIn } = useAuth()
-  if (!isLoggedIn.value && to.path.startsWith('/dashboard')) {
-    return navigateTo('/login')
-  }
-})
